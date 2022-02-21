ef4e65e2bec9e3a393c4fc947a9881f081019122
Sara <lopezlovonsara@gmail.com>
2022-02-21 21:17:47 +0100

    Add component Login

--- client/src/components/Login/Login.jsx
+++ client/src/components/Login/Login.jsx
@@ -1,7 +1,31 @@
 import React from "react";
 
+import logoPrueba  from '../../assets/logoPrueba.jpg';
+
+import './Login.css';
+
 const Login = () => {
-  return <div>Login</div>;
+  return <div>
+    <img src={logoPrueba} alt="logoApp" className="logoApp" /> <br />
+    <form className="formLogin" className="formLogin">
+
+      <label for="userEmail"><b>Email</b></label><br />
+      <input type="text" placeholder="usuario@gmail.com" name="userEmail" className="userEmail" required></input>
+      
+      <br /><br />
+      
+      <label for="userPassword"><b>Password</b></label><br />
+      <input type="password" placeholder="****" name="userPassword" className="userPassword" required></input>
+
+      <br /><br />
+
+      <button type="submit" className="btnLogin">Acceder</button>        
+
+    </form>
+
+    <p className="paragrahLogin">¿No tienes cuenta? <a href="/signup" className="linkRegister" ><b>Regístrate</b></a></p>
+
+  </div>;
 };
 
 export default Login;
