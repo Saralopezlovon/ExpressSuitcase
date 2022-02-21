f9a59aee5a464630aecb8d427271792dced3e2d5
Sara <lopezlovonsara@gmail.com>
2022-02-21 21:47:15 +0100

    Add component menu

--- client/src/components/Menu/Menu.jsx
+++ client/src/components/Menu/Menu.jsx
@@ -1,7 +1,20 @@
 import React from "react";
 
+//import Header from '../Header/Header';
+
+import weekend from '../../assets/img-weekend.jpg';
+import week from '../../assets/img-week.jpg';
+import './Menu.css';
+
+
 const Menu = () => {
-  return <div>Menu</div>;
+  return <div>
+    <div>
+      <p><b>¿Cuánto dura tu viaje?</b></p>
+      <img src={weekend} alt="imageWeekend" className="imageWeekend" height="300" /> <br />
+      <img src={week} alt="imageWeek" className="imageWeek" height="300" /> <br />
+    </div>
+  </div>;
 };
 
 export default Menu;
