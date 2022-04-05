4382a435dc7f261920e3dce235e67cbc1a0312b0
gustavotremont <gustavotremontsr@gmail.com>
2022-04-05 22:53:18 +0200

    add validations to outfits controllers

--- server/controllers/outfits.js
+++ server/controllers/outfits.js
@@ -2,65 +2,93 @@ const Outfits = require('../models/outfits');
 const Suitcases = require('../models/suitcases');
 
 const createOutfit = async (req, res) =>{
- try{
-    const {day, top, bottom, lingerie, shoes} = req.body
+   const {day, top, bottom, lingerie, shoes} = req.body
+   const {suitcaseId} = req.query   
+   let errors = []
+   try{
+      if(!day || !top || !bottom || !lingerie || !shoes){
+         errors.push('Todos los campos deben estar llenos')
+      }
+      if(!suitcaseId){
+         errors.push('La maleta no existe')
+      }
 
-    const {suitcaseId} = req.query   
+      if(errors.length > 0) {
+         res.status(400).json({'errors':errors})
+      }
+      else {
 
-    const newOutfit = await Outfits.create({
-        day: day,
-        top: top,
-        bottom: bottom ,
-        lingerie: lingerie,
-        shoes: shoes,
-        id_suitcase: suitcaseId
+         const newOutfit = await Outfits.create({
+            day: day,
+            top: top,
+            bottom: bottom ,
+            lingerie: lingerie,
+            shoes: shoes,
+            id_suitcase: suitcaseId
+         });
+   
+         const suitcase = await Suitcases.findById(suitcaseId)
+   
+         suitcase.id_outfit.push(newOutfit._id)
+         await suitcase.save()
+   
+         res.status(200).json(newOutfit)
+      }
 
-    });
-
-
-    const suitcase = await Suitcases.findById(suitcaseId)
-
-    suitcase.id_outfit.push(newOutfit._id)
-    await suitcase.save()
-
-    res.status(200).json(newOutfit)
-
- }catch(err){
-    res.status(400).json({'error':err})
- }
+   }catch(err){
+      res.status(400).json({'error':err})
+   }
 };
 
 const updateOutfit = async (req, res) =>{
-    try{
-       const {top, bottom, lingerie, shoes} = req.body
-       const {outfitId} = req.query   
-   
-       const outfit = await Outfits.findById(outfitId);
-        
-       outfit.top = top;
-       outfit.bottom = bottom;
-       outfit.lingerie = lingerie;
-       outfit.shoes = shoes;
+   const {top, bottom, lingerie, shoes} = req.body
+   const {outfitId} = req.query  
+   let errors = [] 
+   try{
 
-       await outfit.save()
+      if(!top || !bottom || !lingerie || !shoes){
+         errors.push('Todos los campos deben estar llenos')
+      }
+      if(!outfitId){
+         errors.push('El conjunto no existe')
+      }
+
+      if(errors.length > 0) {
+         res.status(400).json({'errors':errors})
+      }
+      else {
+         const outfit = await Outfits.findById(outfitId);
+         
+         outfit.top = top;
+         outfit.bottom = bottom;
+         outfit.lingerie = lingerie;
+         outfit.shoes = shoes;
    
-       res.status(200).json(newOutfit)
+         await outfit.save()
    
-    }catch(err){
-       res.status(400).json({'error':err})
-    }
+         res.status(200).json(newOutfit)
+      }
+
+
+   }catch(err){
+      res.status(400).json({'error':err})
+   }
 };
 
 const deleteAllOutfitsBySuitecase = async (req, res) =>{
-    try{
-       const {suitcaseId} = req.query   
-   
-       const outfits = await Outfits.deleteMany({ id_suitcase: suitcaseId });
-       res.status(200).json({msg: 'borrados', outfits})
-   
-    }catch(err){
-       res.status(400).json({'error':err})
-    }
+   const {suitcaseId} = req.query   
+   try{
+
+      if(!suitcaseId) {
+         res.status(400).json({'error': 'La maleta no existe'})
+      }
+      else {
+         const outfits = await Outfits.deleteMany({ id_suitcase: suitcaseId });
+         res.status(200).json({msg: 'borrados', outfits})
+      }
+   }catch(err){
+      res.status(400).json({'error':err})
+   }
 };
 
 
